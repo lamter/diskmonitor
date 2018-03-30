1483e6285d0d1816e52e27872aac9dadea1cf247
lamter <lamter.fu@gmail.com>
2018-03-30 17:35:07 +0800

    feat: 更改json 判定语句

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import logging.config
 import psutil  # pip install psutil
 import json
+from collections import OrderedDict
 
 kwargs_json = 'conf/disk_limit.json'
 logging_conf = 'conf/logging.conf'
@@ -19,12 +20,25 @@ disks = kwargs['disks']
 
 
 def monitor_disk(disk, limit):
-    disk_percent = psutil.disk_usage(disk).percent
-    if disk_percent > limit:
+    disk_usage = psutil.disk_usage(disk)
+    # 进行 GB 换算
+    to_GB = lambda x: round(x / 1000 / 1000 / 1000, 2)
+
+    usage = OrderedDict((
+        ("host", HOST),
+        ("total", to_GB(disk_usage.total)),
+        ("used", to_GB(disk_usage.used)),
+        ("free", to_GB(disk_usage.free)),
+        ("percent", disk_usage.percent),
+    ))
+
+    log = '\nhost:{host}\ttotal:{total}GB\tused:{used}GB\tfree:{free}GB\tpercent:{percent}%'.format(**usage)
+
+    if eval(limit.format(**usage)):
         # 超过显示，进行 warning
-        logger.warning('host:{}\t磁盘:{}\t用量 {}%'.format(HOST, disk, disk_percent))
+        logger.warning(log)
     else:
-        logger.info('host:{}\t磁盘:{}\t用量 {}%'.format(HOST, disk, disk_percent))
+        logger.info(log)
 
 
 for disk, limit in disks.items():
